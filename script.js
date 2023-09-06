9d78313b81be19739671fda0577296230d401fc6
Lya Roux <lya.roux@pm.me>
2023-09-06 19:54:29 +0200

    les mots à taper sont dans un ordre aléatoire et la touche espace reset l input

---
+++ script.js
@@ -0,0 +1,102 @@
+var inputbox = document.getElementById('typeInput');
+var input = inputbox.value;
+var scorebox = document.getElementById('score');
+
+const frenchWords = [
+    "Bonjour",
+    "Merci",
+    "Oui",
+    "Non",
+    "Excusez-moi",
+    "Manger",
+    "Boire",
+    "Maison",
+    "Amour",
+  "Chien",
+  "Chat",
+  "Livre",
+  "École",
+  "Musique",
+  "Soleil",
+  "Lune",
+  "Étoile",
+  "Fleur",
+  "Arbre",
+  "Montagne",
+  "Plage",
+  "Rivière",
+  "Ville",
+  "Pays",
+  "Voiture",
+  "Train",
+  "Avion",
+  "Vélo",
+  "Temps",
+  "Heure",
+  "Jour",
+  "Nuit",
+  "Matin",
+  "Après-midi",
+  "Soir",
+  "Hôtel",
+  "Restaurant",
+  "Vin",
+  "Bière",
+  "Fromage",
+  "Pain",
+  "Eau",
+  "Café",
+  "Thé",
+  "Fruits",
+  "Légumes",
+  "Viande",
+  "Poisson",
+  "Dessert"
+];
+
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+        
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    
+    return array;
+}
+
+function merge(array) {
+    let merged = '';
+    
+    for(i = 0; i < array.length; i++) {
+        merged += array[i] + ' '
+    }
+    return merged;
+}
+
+var wordList = shuffle(frenchWords);
+document.getElementById('words').innerHTML = merge(wordList);
+var i = 0;
+
+addEventListener('keydown', (nextWord)=> {
+    console.log(' ' + wordList[i])
+    if(nextWord.isComposing || nextWord.keyCode === 32) {
+        if (input === ' ' + wordList[i]) {
+            correctWordNumber++;
+            scorebox.value += 1
+
+        }
+        console.log(input == wordList[i])
+        inputbox.value = '';
+        i++
+        console.log(scorebox.value);
+
+    }
+});

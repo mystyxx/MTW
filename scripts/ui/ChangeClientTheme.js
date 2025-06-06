050781f13be3974d751e09988f3c352ee19acf16
Paul Roux <paul.rouxb@proton.me>
2025-06-06 17:45:39 +0200

    ajout de l'alerte pour le focus

--- scripts/ui/ChangeClientTheme.js
+++ scripts/ui/ChangeClientTheme.js
@@ -6,6 +6,7 @@ export function changeClientTheme(theme) {
         document.body.style.setProperty("--secondary-text-color", '#7f7f7f');
         document.body.style.setProperty("--outline-color", '#5f6c7b');
         document.getElementById('words').style.outline = '1px solid';
+        document.body.style.setProperty("--main-bg-color-transparent", 'rgba(255, 255, 255, 0.5)')
         document.body.style.setProperty("--hover-color", '#5f6c7b');
         document.body.style.setProperty("--great-color", '#5f6c7b');
     }
@@ -15,6 +16,7 @@ export function changeClientTheme(theme) {
         document.body.style.setProperty("--secondary-bg-color", '#232F4F');
         document.body.style.setProperty("--outline-color", '#727D82');
         document.getElementById('words').style.outline = '1px solid';
+        document.body.style.setProperty("--main-bg-color-transparent", 'rgba(18, 25, 44, 0.5)')
         document.body.style.setProperty("--secondary-text-color", '#7f7f7f');
         document.body.style.setProperty("--hover-color", '#7E7F91');
         document.body.style.setProperty("--great-color", '#7E7F91');
